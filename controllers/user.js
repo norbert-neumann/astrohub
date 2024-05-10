f777f7b564a55f0787b94e22a2f2f5dd053712cd
norbert-neumann <neumann.norbert.norbert@gmail.com>
2024-05-10 15:11:34 +0200

    separated logic from user router to user controller

---
+++ controllers/user.js
@@ -0,0 +1,136 @@
+function createUserController(repository) {
+    const getUser = async (req, res) => {
+        const user = await repository.getUserById(req.params.userId)
+        res.send(user)
+    }
+
+    const getFriends = async (req, res) => {
+        const user = await repository.getFriends(req.params.userId)
+        res.send(user)
+    }
+
+    const getFriendRequests = async (req, res) => {
+        const user = await repository.getFriendRequests(req.params.userId)
+        res.send(user)
+    }
+
+    const getFavouriteSpots = async (req, res) => {
+        const user = await repository.getFavouriteSpots(req.params.userId)
+        res.send(user)
+    }
+
+    const getTrips = async (req, res) => {
+        const user = await repository.getTrips(req.params.userId)
+        res.send(user)
+    }
+
+    const createUser = async (req, res) => {
+        const user = {
+            username: req.body.username,
+            displayName: req.body.displayName,
+            favouriteSpots: [],
+            trips: [],
+            friends: [],
+            friendRequests: []
+        }
+        repository.saveUser(user)
+        res.send({data: 'POST user'})
+    }
+
+    const addFriend = async (req, res) => {
+        const senderId = req.params.userId
+        const friendId = req.body.friendId
+        repository.addToFriends(senderId, friendId)
+        res.send({data: 'POST add friend'})
+    }
+
+    const addTrip = async (req, res) => {
+        const userId = req.params.userId
+        const tripId = req.body.tripId
+        repository.addToTrips(userId, tripId)
+        res.send({data: 'POST add trip'})
+    }
+
+    const addFavouriteSpot = async (req, res) => {
+        const userId = req.params.userId
+        const spotId = req.body.spotId
+        repository.addToFavouriteSpots(userId, spotId)
+        res.send({data: 'POST add favourite spot'})
+    }
+
+    const addFriendRequest = async (req, res) => {
+        const senderId = req.params.userId
+        const friendId = req.body.friendId
+        repository.addToFriendRequests(friendId, senderId)
+        res.send({data: 'POST add friend request'})
+    }
+
+    const updateUsername = async (req, res) => {
+        let userId = req.params.userId
+        let newUsername = req.body.newUsername
+        let result = await repository.updateUsername(userId, newUsername)
+        res.send(result)
+    }
+
+    const updateDisplayName = async (req, res) => {
+        let userId = req.params.userId
+        let newDisplayName = req.body.newDisplayName
+        let result = await repository.updateDisplayName(userId, newDisplayName)
+        res.send(result)
+    }
+
+    const deleteUser = async (req, res) => {
+        const result = await repository.deleteUser(req.params.userId)
+        res.send(result)
+    }
+
+    const removeFriend = async (req, res) => {
+        const userId = req.params.userId
+        const friendId = req.body.friendId
+        repository.removeFromFriends(userId, friendId)
+        res.send({data: 'DELETE remove friend'})
+    }
+
+    const removeTrip = async (req, res) => {
+        const userId = req.params.userId
+        const tripId = req.body.tripId
+        repository.removeFromTrips(userId, tripId)
+        res.send({data: 'DELETE remove trip'})
+    }
+
+    const removeFavouriteSpot = async (req, res) => {
+        const userId = req.params.userId
+        const spotId = req.body.spotId
+        repository.removeFromFavouriteSpots(userId, spotId)
+        res.send({data: 'DELETE remove favourite spot'})
+    }
+
+    const removeFriendRequest = async (req, res) => {
+        const userId = req.params.userId
+        const senderId = req.body.senderId
+        repository.removeFromFriendRequests(userId, senderId)
+        res.send({data: 'DELETE remove friend request'})
+    }
+
+    return {
+        getUser,
+        getFriends,
+        getFriendRequests,
+        getFavouriteSpots,
+        getTrips,
+        createUser,
+        addFriend,
+        addTrip,
+        addFavouriteSpot,
+        addFriendRequest,
+        updateUsername,
+        updateDisplayName,
+        deleteUser,
+        removeFriend,
+        removeTrip,
+        removeFavouriteSpot,
+        removeFriendRequest
+    }
+}
+
+export default createUserController
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import validate from '../validator.js'
 import { displayNameSchema, userSchema, usernameSchema } from '../schema.js'
+import createUserController from '../controllers/user.js'
 
 function createUserRouter(repository) {
     const router = express.Router()
+    const controller = createUserController(repository)
 
     const extractUserId = (req, res, next) => {
         const userId = req.cookies.userId
         if (userId) {
             req.params.userId = userId
             next()
-        }
-        else {
+        } else {
             res.sendStatus(401)
         }
     }
@@ -19,125 +20,31 @@ function createUserRouter(repository) {
     router.use((req, res, next) => {
         if (req.method !== 'POST' || req.url !== '/') {
             extractUserId(req, res, next)
-        }
-        else {
+        } else {
             next()
         }
     })
 
-    router.get('/:userId', async (req, res) => {
-        const user = await repository.getUserById(req.params.userId)
-        res.send(user)
-    })
+    router.get('/:userId', controller.getUser)
+    router.get('/:userId/friends', controller.getFriends)
+    router.get('/:userId/friend-requests', controller.getFriendRequests)
+    router.get('/:userId/favourite-spots', controller.getFavouriteSpots)
+    router.get('/:userId/trips', controller.getTrips)
 
-    router.get('/:userId/friends', async (req, res) => {
-        const user = await repository.getFriends(req.params.userId)
-        res.send(user)
-    })
+    router.post('/', validate(userSchema), controller.createUser)
+    router.post('/:userId/friends', controller.addFriend)
+    router.post('/:userId/trips', controller.addTrip)
+    router.post('/:userId/favourite-spots', controller.addFavouriteSpot)
+    router.post('/:userId/friend-requests/', controller.addFriendRequest)
 
-    router.get('/:userId/friend-requests', async (req, res) => {
-        const user = await repository.getFriendRequests(req.params.userId)
-        res.send(user)
-    })
+    router.patch('/:userId/username', validate(usernameSchema), controller.updateUsername)
+    router.patch('/:userId/displayName', validate(displayNameSchema), controller.updateDisplayName)
 
-    router.get('/:userId/favourite-spots', async (req, res) => {
-        const user = await repository.getFavouriteSpots(req.params.userId)
-        res.send(user)
-    })
-
-    router.get('/:userId/trips', async (req, res) => {
-        const user = await repository.getTrips(req.params.userId)
-        res.send(user)
-    })
-
-    router.post('/', validate(userSchema), async (req, res) => {
-        const user = {
-            username: req.body.username,
-            displayName: req.body.displayName,
-            favouriteSpots: [],
-            trips: [],
-            friends: [],
-            friendRequests: []
-        }
-        repository.saveUser(user)
-        res.send({data: 'POST user'})
-    })
-
-    router.post('/:userId/friends', async (req, res) => {
-        const senderId = req.params.userId
-        const friendId = req.body.friendId
-        repository.addToFriends(senderId, friendId)
-        res.send({data: 'POST add friend'})
-    })
-
-    router.post('/:userId/trips', async (req, res) => {
-        const userId = req.params.userId
-        const tripId = req.body.tripId
-        repository.addToTrips(userId, tripId)
-        res.send({data: 'POST add trip'})
-    })
-
-    router.post('/:userId/favourite-spots', async (req, res) => {
-        const userId = req.params.userId
-        const spotId = req.body.spotId
-        repository.addToFavouriteSpots(userId, spotId)
-        res.send({data: 'POST add favourite spot'})
-    })
-
-    router.post('/:userId/friend-requests/', async (req, res) => {
-        const senderId = req.params.userId
-        const friendId = req.body.friendId
-        repository.addToFriendRequests(friendId, senderId)
-        res.send({data: 'POST add friend request'})
-    })
-
-    // TODO: patch esetén is okés req.body-t használni?
-    router.patch('/:userId/username', validate(usernameSchema), async (req, res) => {
-        let userId = req.params.userId
-        let newUsername = req.body.newUsername
-        let result = await repository.updateUsername(userId, newUsername)
-        res.send(result)
-    })
-
-    router.patch('/:userId/displayName', validate(displayNameSchema), async (req, res) => {
-        let userId = req.params.userId
-        let newDisplayName = req.body.newDisplayName
-        let result = await repository.updateDisplayName(userId, newDisplayName)
-        res.send(result)
-    })
-
-    router.delete('/:userId', async (req, res) => {
-        const result = await repository.deleteUser(req.params.userId)
-        res.send(result)
-    })
-
-    router.delete('/:userId/friend', async (req, res) => {
-        const userId = req.params.userId
-        const friendId = req.body.friendId
-        repository.removeFromFriends(userId, friendId)
-        res.send({data: 'DELETE remove friend'})
-    })
-
-    router.delete('/:userId/trips', async (req, res) => {
-        const userId = req.params.userId
-        const tripId = req.body.tripId
-        repository.removeFromTrips(userId, tripId)
-        res.send({data: 'DELETE remove trip'})
-    })
-
-    router.delete('/:userId/favourite-spots', async (req, res) => {
-        const userId = req.params.userId
-        const spotId = req.body.spotId
-        repository.removeFromFavouriteSpots(userId, spotId)
-        res.send({data: 'DELETE remove  favourite spot'})
-    })
-
-    router.delete('/:userId/friend-requests', async (req, res) => {
-        const userId = req.params.userId
-        const senderId = req.body.senderId
-        repository.removeFromFriendRequests(userId, senderId)
-        res.send({data: 'DELETE remove  friend request'})
-    })
+    router.delete('/:userId', controller.deleteUser)
+    router.delete('/:userId/friend', controller.removeFriend)
+    router.delete('/:userId/trips', controller.removeTrip)
+    router.delete('/:userId/favourite-spots', controller.removeFavouriteSpot)
+    router.delete('/:userId/friend-requests', controller.removeFriendRequest)
 
     return router
 }
